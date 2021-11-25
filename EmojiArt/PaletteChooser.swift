64474ff861afa9d04bee571bd8248c43003b962a
Ramona Marti <ramona.marti@students.fhnw.ch>
2021-11-25 14:21:45 +0100

    - button for editing palette - add emojis to palette with input field - change name of palette with input field

--- EmojiArt/PaletteChooser.swift
+++ EmojiArt/PaletteChooser.swift
@@ -10,20 +10,30 @@ import SwiftUI
 struct PaletteChooser: View {
     @ObservedObject var document: EmojiArtDocumentViewModel
     @Binding var chosenPalette: String
+    @State private var isPaletteEditorPresented = false
 
     var body: some View {
         HStack {
             Button(action: {
                 chosenPalette = document.palette(before: chosenPalette)
             }) {
-                Image(systemName: "arrow.left")
+                Image(systemName: "arrow.left").imageScale(.large)
             }
             Button(action: {
                 chosenPalette = document.palette(after: chosenPalette)
             }) {
                 Image(systemName: "arrow.right")
             }
-            Text(document.paletteNames[chosenPalette] ?? "")
+            Text(document.paletteNames[chosenPalette] ?? "").imageScale(.large)
+            Button {
+                isPaletteEditorPresented = true
+            } label: {
+                Image(systemName: "square.and.pencil").imageScale(.large)
+            }
+            .popover(isPresented: $isPaletteEditorPresented) {
+                PaletteEditor(chosenPalette: $chosenPalette, document: document)
+                    .frame(width: 300, height: 500, alignment: .top)
+            }
         }
     }
 }
