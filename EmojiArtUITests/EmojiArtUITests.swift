1e6baa321c2e7b2954b1f047606349b4815e2579
Ramona Marti <ramona.marti@students.fhnw.ch>
2022-01-14 00:48:55 +0100

    - add ui test part 1
    TODO: type keys not working

--- EmojiArtUITests/EmojiArtUITests.swift
+++ EmojiArtUITests/EmojiArtUITests.swift
@@ -10,30 +10,41 @@ import XCTest
 class EmojiArtUITests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        super.setUp()
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    func testEditDocumentTitle() throws {
         let app = XCUIApplication()
         app.launch()
+        
+        if app.navigationBars["Untitled"].exists {
+            app.navigationBars["Untitled"]/*@START_MENU_TOKEN@*/.buttons["BackButton"]/*[[".buttons[\"Emoji Art\"]",".buttons[\"BackButton\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        }
+        let emojiArtNavigationBar = app.navigationBars["Emoji Art"]
+        emojiArtNavigationBar.buttons["Edit"].tap()
+        app.tables.textFields["Untitled"].tap()
+        app.tables.textFields["Untitled"].doubleTap()
+//        app.keys["N"].tab()
+//        app.keys["e"].tab()
+//        app.keys["w"].tab()
+//        app.keys[" "].tab()
+//        app.keys["T"].tab()
+//        app.keys["i"].tab()
+//        app.keys["t"].tab()
+//        app.keys["l"].tab()
+//        app.keys["e"].tab()
+        emojiArtNavigationBar.buttons["Done"].tap()
+        app.tables.buttons["New Title"].tap()
 
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
+    func estLaunchPerformance() throws {
+        if #available(iOS 13.0, *) {
             measure(metrics: [XCTApplicationLaunchMetric()]) {
                 XCUIApplication().launch()
             }
