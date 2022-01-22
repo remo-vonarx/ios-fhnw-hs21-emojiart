2a104259e36fa923f22abe11ba53cafb4ec7542a
Remo von Arx <remo.vonarx@students.fhnw.ch>
2022-01-22 14:25:46 +0100

    Task 2: - Rework  createPreviewForDocument to a struct because the preview was not being updated when the document changed - Move struct to EmojiArtDocumentPreview.swift

---
+++ EmojiArt/Overview/EmojiArtDocumentPreview.swift
@@ -0,0 +1,40 @@
+//
+//  EmojiArtDocumentPreview.swift
+//  EmojiArt
+//
+//  Created by Remo von Arx on 22.01.22.
+//
+
+import SwiftUI
+
+struct EmojiArtDocumentPreview: View {
+    @ObservedObject var document: EmojiArtDocumentViewModel
+    var documentName: String
+    @State private var customFont: Font?
+
+    var body: some View {
+        VStack {
+            if let image = document.backgroundImage {
+                Image(uiImage: image)
+                    .resizable()
+                    .aspectRatio(ratio, contentMode: .fit)
+                    .overlay(RoundedRectangle(cornerRadius: corner).stroke(.black, lineWidth: lineWidth))
+                    .opacity(document.opacityImage)
+            } else {
+                Image("Placeholder_ArtWall")
+                    .resizable()
+                    .aspectRatio(ratio, contentMode: .fit)
+                    .overlay(RoundedRectangle(cornerRadius: corner).stroke(.black, lineWidth: lineWidth))
+            }
+            Text(documentName).font(customFont)
+            
+            Text("Time spent: \(document.timeSpentFormatted)").font(customFont).foregroundColor(.black)
+        }
+    }
+}
+
+// MARK: - Drawing constants
+
+private let ratio: CGFloat = 1
+private let corner: CGFloat = 4
+private let lineWidth: CGFloat = 2
--- EmojiArt/Overview/EmojiArtWallView.swift
+++ EmojiArt/Overview/EmojiArtWallView.swift
@@ -29,36 +29,14 @@ struct EmojiArtWallView: View {
                         .navigationTitle(name)
                         .font(customFont)
                     NavigationLink(destination: emojiArtDocumentView) {
-                        createPreviewForDocument(document: document, documentName: name)
+                        EmojiArtDocumentPreview(document: document, documentName: name).font(customFont)
                     }.font(customFont)
                 }
             }.padding(10)
         }
     }
-    
-    fileprivate func createPreviewForDocument(document: EmojiArtDocumentViewModel, documentName: String) -> some View {
-        VStack {
-            if let image = document.backgroundImage {
-                Image(uiImage: image)
-                    .resizable()
-                    .aspectRatio(ratio, contentMode: .fit)
-                    .overlay(RoundedRectangle(cornerRadius: corner).stroke(.black, lineWidth: lineWidth))
-                    .opacity(document.opacityImage)
-            } else {
-                Image("Placeholder_ArtWall")
-                    .resizable()
-                    .aspectRatio(ratio, contentMode: .fit)
-                    .overlay(RoundedRectangle(cornerRadius: corner).stroke(.black, lineWidth: lineWidth))
-            }
-            Text(documentName).font(customFont)
-        }
-    }
 }
-
 
 // MARK: - Drawing constants
 
 private let columnSize: CGFloat = 50
-private let ratio: CGFloat = 1
-private let corner: CGFloat = 4
-private let lineWidth: CGFloat = 2
